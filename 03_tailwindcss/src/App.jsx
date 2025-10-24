5fa3281db8baa084cfdf45694513384434e7275b
Irish <irish1723@gmail.com>
2025-10-24 15:32:11 +0530

    talwind

--- 03_tailwindcss/src/App.jsx
+++ 03_tailwindcss/src/App.jsx
@@ -8,9 +8,10 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold text-blue-600 bg-gray-100 p-4">
         Tailwind is Working!
       </h1>
+      <img src="https://plus.unsplash.com/premium_photo-1671683370315-87306b0faf90?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1172" alt="" />
     </>
   )
 }
